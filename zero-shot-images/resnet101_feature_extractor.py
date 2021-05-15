30c253cd4a20d2c27561695083cd12ccb4bca3e3
e96031413 <30921855+e96031413@users.noreply.github.com>
2021-05-15 08:23:56 +0800

    Update resnet101_feature_extractor.py

--- zero-shot-images/resnet101_feature_extractor.py
+++ zero-shot-images/resnet101_feature_extractor.py
@@ -13,9 +13,9 @@ from PIL import Image
 from tqdm import tqdm
 
 # data_dir = "./test"
-data_dir = "/root/notebooks/nfs/work/dataset/XXison/datasets_for_ma/"
+data_dir = "../XXison/datasets_for_ma/"
 
-dataset_info = json.load(open("../../dataset/XXison/dataset.json", "r"))
+dataset_info = json.load(open("../XXison/dataset.json", "r"))
 df = pd.DataFrame.from_dict(dataset_info, orient="index")
 df['file_name'] = df.index
 df["file_name"] = data_dir + df["file_name"].astype(str)   #新增圖片完整路徑(full path)
@@ -80,7 +80,8 @@ file_path_list = []
 label_list = []
 
 files = df["file_name"].tolist()          #之前用os.walk去訪歷資料夾，但是百萬張圖片的速度太慢，透過之前預先建好的json檔案，直接取得路徑
-for file_path in tqdm(files):                    
+
+for file_path in tqdm(files[0:10]):                    
     if (is_image(file_path)):             #假如路徑是圖片
         image = Image.open(file_path)     #開圖檔
         pic_vector = get_vector(image)    #用ResNet101萃取圖片的特徵向量
@@ -89,13 +90,28 @@ for file_path in tqdm(files):
         feature_list.append(pic_vector.data.tolist())    #把該張圖片萃取的特徵加到list保存      
         
         #如果當前萃取之圖片的檔名與原始json檔中的檔名相符，取得該檔名的class值，並加到list保存
-        label_list.append(orig_df.loc[orig_df['file_name'] == file_path.split("/")[-1] ]['class'].values[0]) 
-        print(file_path+" Done!")   #萃取完成顯示Done
+        label_list.append(orig_df.loc[orig_df['file_name'] == file_path.split("/")[-1] ]['class'].values[0])       
         
-        
+for index, value in enumerate(label_list):
+    if value == 'good':
+        label_list[index] = 1
+    elif value == 'missing':
+        label_list[index] = 2
+    elif value == 'shift':
+        label_list[index] = 3
+    elif value == 'stand':
+        label_list[index] = 4
+    elif value == 'broke':
+        label_list[index] = 5
+    elif value == 'short':
+        label_list[index] = 6
+
+# https://stackoverflow.com/a/11885718/13369757 (單維度dim轉置)
+# https://stackoverflow.com/a/7464816 (處理char字串呈現問題)
 
 feat['features'] = np.array(feature_list).astype('float32').T
-feat['image_files'] = np.array(file_path_list)
-feat['labels'] = np.array(label_list).astype('float32')
+feat['image_files'] = np.array(file_path_list, dtype=np.object)[None].T
+feat['labels'] = np.array(label_list).astype('float32')[None].T
+
 
 io.savemat('res101.mat',feat)
