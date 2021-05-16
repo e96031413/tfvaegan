4d0512c6ce98155b9e8ba37fbcf90d43cd5bbe90
e96031413 <30921855+e96031413@users.noreply.github.com>
2021-05-16 09:29:13 +0800

    Update resnet101_feature_extractor.py

--- zero-shot-images/resnet101_feature_extractor.py
+++ zero-shot-images/resnet101_feature_extractor.py
@@ -11,6 +11,7 @@ from torch.utils import data
 import os
 from PIL import Image
 from tqdm import tqdm
+import h5py
 
 # data_dir = "./test"
 data_dir = "../XXison/datasets_for_ma/"
@@ -28,6 +29,20 @@ print("Load dataset.json successfully!!!")
 transforms = torchvision.transforms.Compose([
     torchvision.transforms.ToTensor(),
 ])
+
+def save_dict_to_hdf5(dic, filename):
+    with h5py.File(filename, 'w') as h5file:
+        recursively_save_dict_contents_to_group(h5file, '/', dic)
+
+def recursively_save_dict_contents_to_group(h5file, path, dic):
+    for key, item in dic.items():
+        if isinstance(item, (np.ndarray, np.int64, np.float64, str, bytes,'<U44')):
+            h5file[path + key] = item
+        elif isinstance(item, dict):
+            recursively_save_dict_contents_to_group(h5file, path + key + '/', item)
+        else:
+            raise ValueError('Cannot save %s type'%type(item))
+
 
 def is_image(f):
     return f.endswith(".png") or f.endswith(".jpg")
@@ -80,8 +95,8 @@ file_path_list = []
 label_list = []
 
 files = df["file_name"].tolist()          #之前用os.walk去訪歷資料夾，但是百萬張圖片的速度太慢，透過之前預先建好的json檔案，直接取得路徑
-
-for file_path in tqdm(files[0:10]):                    
+# files = ['/root/notebooks/nfs/work/yanwei.liu/Phison/test/1.jpg','/root/notebooks/nfs/work/yanwei.liu/Phison/test/2.jpg','/root/notebooks/nfs/work/yanwei.liu/Phison/test/3.jpg']
+for file_path in tqdm(files[0:700000]):                    
     if (is_image(file_path)):             #假如路徑是圖片
         image = Image.open(file_path)     #開圖檔
         pic_vector = get_vector(image)    #用ResNet101萃取圖片的特徵向量
@@ -109,9 +124,17 @@ for index, value in enumerate(label_list):
 # https://stackoverflow.com/a/11885718/13369757 (單維度dim轉置)
 # https://stackoverflow.com/a/7464816 (處理char字串呈現問題)
 
-feat['features'] = np.array(feature_list).astype('float32').T
+"""
+feat['features'] = np.array(feature_list).astype('float32').T                #小檔案(2GB)以內，可用這4行註解的，直接產生mat檔案
 feat['image_files'] = np.array(file_path_list, dtype=np.object)[None].T
 feat['labels'] = np.array(label_list).astype('float32')[None].T
-
-
 io.savemat('res101.mat',feat)
+"""
+
+# 由於scipy無法將大量的矩陣寫入到 mat檔案的關係，因此改用hdf5格式保存
+# 到Octave開啟進行Transpose後，將多個h5合併成1個h5後，再保存成v7版本的mat檔案，最後透過tfvaegan進行訓練
+
+feat['features'] = np.array(feature_list).astype('float32')                    #大檔案(2GB)以上，則使用以下程式保存hdf5格式
+# feat['image_files'] = np.array(file_path_list, dtype=np.object)              #為降低空間占用，不保存檔案路徑到hdf5
+feat['labels'] = np.array(label_list).astype('float32')[None].T                #於Octave開啟後，記得將labels的變數進行Transpose
+save_dict_to_hdf5(feat, 'res101.h5')
